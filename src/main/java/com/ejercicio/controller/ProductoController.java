df8685d5b8a978933b81536ef2a43215dc4a03ae
Jx64 <jesusdv09@gmail.com>
2024-04-03 08:04:52 -0500

    remove mapper folder in dto and update dto with data

--- src/main/java/com/ejercicio/controller/ProductoController.java
+++ src/main/java/com/ejercicio/controller/ProductoController.java
@@ -1,6 +1,5 @@
 package com.ejercicio.controller;
 
-import com.ejercicio.dto.ProductoDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
