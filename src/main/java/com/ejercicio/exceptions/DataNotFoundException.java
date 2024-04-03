a6efe2e75b62417372ec71a7136d04d988275529
Jx64 <jesusdv09@gmail.com>
2024-04-03 07:57:38 -0500

    Update DataNotFoundException.java

--- src/main/java/com/ejercicio/exceptions/DataNotFoundException.java
+++ src/main/java/com/ejercicio/exceptions/DataNotFoundException.java
@@ -7,19 +7,4 @@ public class DataNotFoundException extends RuntimeException {
     public DataNotFoundException(String message) {
         super(message);
     }
-<<<<<<< HEAD
-=======
-
-    public DataNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DataNotFoundException(Throwable cause) {
-        super(cause);
-    }
-
-    public DataNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
->>>>>>> origin/main
 }
