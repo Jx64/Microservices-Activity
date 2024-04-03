f63cb2f790c7b5dfcea6dd4d8ccffe7bf17fdea4
Jx64 <jesusdv09@gmail.com>
2024-04-03 15:13:16 -0500

    Update application.properties

--- src/main/java/com/ejercicio/EjercicioApplication.java
+++ src/main/java/com/ejercicio/EjercicioApplication.java
@@ -2,15 +2,8 @@ package com.ejercicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 
 @SpringBootApplication()
-=======
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@SpringBootApplication
->>>>>>> origin/main
 public class EjercicioApplication {
 
     public static void main(String[] args) {
